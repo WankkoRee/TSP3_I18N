8310275dce50ac1d87b1f1246f999e2d59acc5c3
Wankko Ree <wkr@wkr.moe>
2022-05-08 16:51:47 +0800

    项目初始化

---
+++ TheStanleyParableUltraDeluxe_Chinese/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("TheStanleyParableUltraDeluxe_Chinese")]
+[assembly: AssemblyDescription("汉化《史丹利的寓言：终极豪华版》所使用的字库插件，基于 BepInEx 和 I18NFont4UnityGame")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheStanleyParableUltraDeluxe_Chinese")]
+[assembly: AssemblyCopyright("Copyright © by Wankko Ree")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("262c7f82-6682-465b-a1aa-114e3ef85ed6")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
